2d73f6510dd225743f29832f96487de06b8167cf
Yuchen Wang <yuchenwa@andrew.cmu.edu>
2019-10-13 13:05:47 -0400

    Data quality of mongodb to features

--- inference/feature_extraction/extract_features.py
+++ inference/feature_extraction/extract_features.py
@@ -2,18 +2,35 @@ import pandas as pd
 import time
 import os
 import sys
+# import random
 
 import utils.mongodb_client as mongodb_client
 
 rate = mongodb_client.get_rate_table()
+THRESHOLD = 0.2
 
 def extract_user_movie_score():
+    count = 0
+    total = 0
     d = {'user_id': [], 'movie_id': [], 'score': []}
     rate = mongodb_client.get_rate_table()
     for record in rate.find():
-        d['user_id'].append(int(record['user_id']))
-        d['movie_id'].append(int(record['movie_id']))
-        d['score'].append(int(record['score']))
+        total += 1
+        try:
+            user_id, movie_id, score = int(record['user_id']), int(record['movie_id']), int(record['score'])
+            # if (random.random() > 0.4):
+            #     raise ValueError
+        except:
+            print("Something wrong with a line of MongoDB data.")
+            count += 1
+        else:
+            d['user_id'].append(user_id)
+            d['movie_id'].append(movie_id)
+            d['score'].append(score)
+
+    if (count > total * THRESHOLD):
+        # Do something. Such as sending emails to all developers.
+        print("Warning: Possible data schema change or too much missing data")
     df = pd.DataFrame.from_dict(d)
     
     file_name = 'user_movie_score_' + str(int(time.time())) + '.csv'
