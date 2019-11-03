ddf7bac3d379427be24a77bea0c1c2f388bf679e
Chenxi Li <chenxili@andrew.cmu.edu>
2019-11-02 23:28:23 -0400

    update train script remove sudo

--- script/train_evaluate.sh
+++ script/train_evaluate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#sudo python3 ../inference/RecommendModule/operate.py /home/teama/17645TeamA/  offl-exp-1 Train Evaluation
-sudo python3 ../inference/RecommendModule/operate.py /var/lib/jenkins/workspace/DailyTraining/  offl-exp-1 Train Evaluation
+#python3 ../inference/RecommendModule/operate.py /home/teama/17645TeamA/  offl-exp-1 Train Evaluation
+python3 ../inference/RecommendModule/operate.py /var/lib/jenkins/workspace/DailyTraining/  offl-exp-1 Train Evaluation
