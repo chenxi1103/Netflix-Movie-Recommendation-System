06128508b1047e8680d33c4f5eb4739895279855
Chenxi Li <chenxili@andrew.cmu.edu>
2019-11-02 23:15:24 -0400

    update train evaluate script

--- script/train_evaluate.sh
+++ script/train_evaluate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-python3 ../inference/RecommendModule/operate.py /home/teama/17645TeamA/  offl-exp-1 Train Evaluate
+#sudo python3 ../inference/RecommendModule/operate.py /home/teama/17645TeamA/  offl-exp-1 Train Evaluation
+sudo python3 ../inference/RecommendModule/operate.py /var/lib/jenkins/workspace/DailyTraining/  offl-exp-1 Train Evaluation
